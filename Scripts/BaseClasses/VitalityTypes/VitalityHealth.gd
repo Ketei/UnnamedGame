6bdb030f6571b3846d25f0de4e5463603df76224
Ketei <137458469+Ketei@users.noreply.github.com>
2023-08-16 11:48:48 -0600

    Clean and add functionality
    - Removed unnecesary methods to update variables
    - Added function to work with lust
    - Added bool to trigger with module manager
    - Renamed signals and added info to them.

--- Scripts/BaseClasses/VitalityTypes/VitalityHealth.gd
+++ Scripts/BaseClasses/VitalityTypes/VitalityHealth.gd
@@ -1,124 +1,96 @@
 extends Node
 class_name VitalityHealth
 
-signal depleted_health
-signal depleted_stamina
-signal depleted_mana
+signal changed_health(CurrentHealth, MaxHealth)
+signal changed_stamina(CurrentStamina, MaxStamina)
+signal changed_mana(CurrentMana, MaxMana)
 
 var enabled: bool = true
+
+var change_self_with_lust: bool = false
 
 ## Max amount of damage an actor can take.
 var base_health: int = 0 :
 	set(value):
 		if enabled:
 			base_health = maxi(value, 1)
-			update_max_health()
+			max_health = ActorLibs.calculate_stati(base_health, mod_health, mult_health)
+var mod_health: int = 0 :
+	set(value):
+		if enabled:
+			mod_health = value
+			max_health = ActorLibs.calculate_stati(base_health, mod_health, mult_health)
+var mult_health: float = 1.0 :
+	set(value):
+		if enabled:
+			mult_health = value
+			max_health = ActorLibs.calculate_stati(base_health, mod_health, mult_health)
+var max_health: int :
+	set(value):
+		if enabled:
+			max_health = maxi(value, 1)
+			changed_health.emit(health, max_health)
+
 ## Max amount of energy the actor has. Used for attacks, dodges and others.
 var base_stamina: int = 0:
 	set(value):
 		if enabled:
 			base_stamina = maxi(value, 1)
-			update_max_stamina()
+			max_stamina = ActorLibs.calculate_stati(base_stamina, mod_stamina, mult_stamina)
+var mod_stamina: int = 0 :
+	set(value):
+		if enabled:
+			mod_stamina = value
+			max_stamina = ActorLibs.calculate_stati(base_stamina, mod_stamina, mult_stamina)
+var mult_stamina: float = 1.0 :
+	set(value):
+		if enabled:
+			mult_stamina = value
+			max_stamina = ActorLibs.calculate_stati(base_stamina, mod_stamina, mult_stamina)
+var max_stamina: int :
+	set(value):
+		if enabled:
+			max_stamina = maxi(value, 1)
+			changed_stamina.emit(stamina, max_stamina)
+
 ## Max amount of magic the actor has. Used to cast spells.
 var base_mana: int = 0:
 	set(value):
 		if enabled:
 			base_mana = maxi(value, 1)
-			update_max_mana()
-
-var mod_max_health: int = 0 :
+			max_mana = ActorLibs.calculate_stati(base_mana, max_mana, mult_mana)
+var mod_mana: int = 0 :
 	set(value):
 		if enabled:
-			mod_max_health = value
-			update_max_health()
-
-var mod_max_stamina: int = 0 :
+			mod_mana = value
+			max_mana = ActorLibs.calculate_stati(base_mana, max_mana, mult_mana)
+var mult_mana: float = 1.0 :
 	set(value):
 		if enabled:
-			mod_max_stamina = value
-			update_max_stamina()
-
-var mod_max_mana: int = 0 :
-	set(value):
-		if enabled:
-			mod_max_mana = value
-			update_max_stamina()
-
-# Multipliers
-# Multipliers are used to reduce or buff stats by %.
-# Ex. [1.0 * 0.5 * 0.2] = 10%
-var mult_health: Array = []
-var mult_stamina: Array = []
-var mult_mana: Array = []
-
-# Max stats values
-var max_health: int :
-	set(value):
-		if enabled:
-			max_health = maxi(value, 1)
-
-var max_stamina: int :
-	set(value):
-		if enabled:
-			max_stamina = maxi(value, 1)
-
+			mult_mana = value
+			max_mana = ActorLibs.calculate_stati(base_mana, max_mana, mult_mana)
 var max_mana: int :
 	set(value):
 		if enabled:
 			max_mana = maxi(value, 1)
+			changed_mana.emit(mana, max_mana)
 
 # Dynamic values
 var health: int :
 	set(value):
 		if enabled:
 			health = clampi(value, 0, max_health)
-			if health == 0:
-				depleted_health.emit()
-
+			changed_health.emit(health, max_health)
 var stamina: int :
 	set(value):
 		if enabled:
 			stamina = clampi(value, 0, max_stamina)
-			if stamina == 0:
-				depleted_stamina.emit()
-
+			changed_stamina.emit(stamina, max_stamina)
 var mana: int :
 	set(value):
 		if enabled:
 			mana = clampi(value, 0, max_mana)
-			if mana == 0:
-				depleted_mana.emit()
-
-
-# Change so that increases additions multiply by difficulty.
-func update_max_health() -> void:
-	var _new_val = floori(float(base_health + mod_max_health) * QuickMath.calculate_multiplier(mult_health))
-	var _diff = _new_val - max_health
-	max_health = _new_val
-	if max_health < health:
-		health = max_health
-	if 0 < _diff:
-		health += _diff
-
-# Change so that increases additions multiply by difficulty.
-func update_max_stamina() -> void:
-	var _new_val = floori(float(base_stamina + mod_max_stamina) * QuickMath.calculate_multiplier(mult_stamina))
-	var _diff = _new_val - max_stamina
-	max_stamina = _new_val
-	if max_stamina < stamina:
-		stamina = max_stamina
-	if 0 < _diff:
-		stamina += _diff
-
-# Change so that increases additions multiply by difficulty.
-func update_max_mana() -> void:
-	var _new_val = floori(float(base_mana + mod_max_mana) * QuickMath.calculate_multiplier(mult_mana))
-	var _diff = _new_val - max_mana
-	max_mana = _new_val
-	if max_mana < mana:
-		mana = max_mana
-	if 0 < _diff:
-		mana += _diff
+			changed_mana.emit(mana, max_mana)
 
 
 func full_restore() -> void:
@@ -126,3 +98,8 @@ func full_restore() -> void:
 	stamina = max_stamina
 	mana = max_mana
 
+
+func trigger_lust_stats_change(CurrentLust: int, PreviousLust: int) -> void:
+	mod_health += SexLibs.get_stat_with_lusti("health", CurrentLust, PreviousLust)
+	mod_stamina += SexLibs.get_stat_with_lusti("stamina", CurrentLust, PreviousLust)
+	mod_mana += SexLibs.get_stat_with_lusti("mana", CurrentLust, PreviousLust)
