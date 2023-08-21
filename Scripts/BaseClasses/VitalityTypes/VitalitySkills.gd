27e1f3b343bdb664dcdb625b9d3ad2e6e2496414
Ketei <137458469+Ketei@users.noreply.github.com>
2023-08-21 15:20:07 -0600

    Removed irrelevant variables
    Removed variables that were implemented in the combat module.

--- Scripts/BaseClasses/VitalityTypes/VitalitySkills.gd
+++ Scripts/BaseClasses/VitalityTypes/VitalitySkills.gd
@@ -6,25 +6,29 @@ var enabled: bool = true
 var change_self_with_lust: int = false
 
 ## Affects physical damage
-var base_strenght: int = 0 :
+var base_strength: int = 0 :
 		set(value):
 			if enabled:
-				base_strenght = maxi(value, 0)
-				strength = ActorLibs.calculate_stati(base_strenght, mod_strength, mult_strength)
+				base_strength = maxi(value, 0)
+				strength = ActorLibs.calculate_stati(base_strength, mod_strength, mult_strength)
 var mod_strength: int = 0 :
 	set(value):
 		if enabled:
 			mod_strength = value
-			strength = ActorLibs.calculate_stati(base_strenght, mod_strength, mult_strength)
+			strength = ActorLibs.calculate_stati(base_strength, mod_strength, mult_strength)
 var mult_strength: float = 1.0 :
 	set(value):
 		if enabled:
 			mult_strength = maxf(value, 0.0)
-			strength = ActorLibs.calculate_stati(base_strenght, mod_strength, mult_strength)
+			strength = ActorLibs.calculate_stati(base_strength, mod_strength, mult_strength)
+var max_strength: int = 0 :
+	set(value):
+		if enabled:
+			max_strength = maxi(value, 0)
 var strength: int = 0:
 	set(value):
 		if enabled:
-			strength = maxi(value, 0)
+			strength = clampi(value, 0, max_strength)
 
 ## Affects your damage reduction & tolerance to sexual attacks
 var base_endurance: int = 0 :
@@ -42,10 +46,14 @@ var mult_endurance: float = 1.0 :
 		if enabled:
 			mult_endurance = maxf(value, 0.0)
 			endurance = ActorLibs.calculate_stati(base_endurance, mod_endurance, mult_endurance)
+var max_endurance: int = 0 :
+	set(value):
+		if enabled:
+			max_endurance = maxi(value, 0)
 var endurance: int = 0 :
 	set(value):
 		if enabled:
-			endurance = maxi(value, 0)
+			endurance = clampi(value, 0, max_endurance)
 
 ## Affects NPC interactions & Store prices.
 var base_charisma: int = 0 :
@@ -63,10 +71,14 @@ var mult_charisma: float = 1.0 :
 		if enabled:
 			mult_charisma = maxf(value, 0.0)
 			charisma = ActorLibs.calculate_stati(base_charisma, mod_charisma, mult_charisma)
+var max_charisma: int = 0 :
+	set(value):
+		if enabled:
+			max_charisma = maxi(value, 0)
 var charisma: int = 0 :
 	set(value):
 		if enabled:
-			charisma = maxi(value, 0)
+			charisma = clampi(value, 0, max_charisma)
 
 ## Affects magic damage
 var base_intelligence: int = 0 :
@@ -84,10 +96,14 @@ var mult_intelligence: float = 1.0 :
 		if enabled:
 			mult_intelligence = maxf(value, 0.0)
 			intelligence = ActorLibs.calculate_stati(base_intelligence, mod_intelligence, mult_intelligence)
+var max_intelligence: int = 0 :
+	set(value):
+		if enabled:
+			max_intelligence = maxi(value, 0)
 var intelligence: int = 0 :
 	set(value):
 		if enabled:
-			intelligence = maxi(value, 0)
+			intelligence = clampi(value, 0, max_intelligence)
 
 ## Affects critical hits & Minigame odds
 var base_luck: int = 0 :
@@ -104,52 +120,14 @@ var mult_luck: float = 1.0 :
 	set(value):
 		mult_luck = maxf(value, 0.0)
 		luck = ActorLibs.calculate_stati(base_luck, mod_luck, mult_luck)
+var max_luck: int = 0 :
+	set(value):
+		if enabled:
+			max_luck = maxi(value, 0)
 var luck: int = 0 :
 	set(value):
 		if enabled:
-			luck = maxi(value, 0)
-
-## Reduces physical damage received
-var base_defense_physical: int = 0 :
-	set(value):
-		if enabled:
-			base_defense_physical = maxi(value, 0)
-			defense_physical = ActorLibs.calculate_stati(base_defense_physical, mod_defense_physical, mult_defense_physical)
-var mod_defense_physical: int = 0 :
-	set(value):
-		if enabled:
-			mod_defense_physical = value
-			defense_physical = ActorLibs.calculate_stati(base_defense_physical, mod_defense_physical, mult_defense_physical)
-var mult_defense_physical: float = 1.0 :
-	set(value):
-		if enabled:
-			mult_defense_physical = maxf(value, 0.0)
-			defense_physical = ActorLibs.calculate_stati(base_defense_physical, mod_defense_physical, mult_defense_physical)
-var defense_physical: int = 0 :
-	set(value):
-		if enabled:
-			defense_physical = maxi(value, 0)
-
-## Reduces magical damage received
-var base_defense_magical: int = 0 :
-	set(value):
-		if enabled:
-			base_defense_magical = maxi(value, 0)
-			defense_magical = ActorLibs.calculate_stati(base_defense_magical, mod_defense_magical, mult_defense_magical)
-var mod_defense_magical: int = 0 :
-	set(value):
-		if enabled:
-			mod_defense_magical = value
-			defense_magical = ActorLibs.calculate_stati(base_defense_magical, mod_defense_magical, mult_defense_magical)
-var mult_defense_magical: float = 0.0 :
-	set(value):
-		if enabled:
-			mult_defense_magical = maxf(value, 0.0)
-			defense_magical = ActorLibs.calculate_stati(base_defense_magical, mod_defense_magical, mult_defense_magical)
-var defense_magical: int = 0 :
-	set(value):
-		if enabled:
-			defense_magical = maxi(value, 0)
+			luck = clampi(value, 0, max_luck)
 
 
 func trigger_lust_stats_change(CurrentLust, PreviousLust) -> void:
@@ -158,7 +136,4 @@ func trigger_lust_stats_change(CurrentLust, PreviousLust) -> void:
 	mod_charisma += SexLibs.get_stat_with_lusti("charisma", CurrentLust, PreviousLust)
 	mod_intelligence += SexLibs.get_stat_with_lusti("intelligence", CurrentLust, PreviousLust)
 	mod_luck += SexLibs.get_stat_with_lusti("luck", CurrentLust, PreviousLust)
-	
-	mod_defense_magical += SexLibs.get_stat_with_lusti("defense-magical", CurrentLust, PreviousLust)
-	mod_defense_physical += SexLibs.get_stat_with_lusti("defense-physical", CurrentLust, PreviousLust)
 
