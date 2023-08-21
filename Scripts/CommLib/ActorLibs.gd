86f2d474c1174ab3ac0accd23900cae231a6be25
Ketei <137458469+Ketei@users.noreply.github.com>
2023-08-21 15:24:10 -0600

    Created combat methods
    - Created method to calculate damage.
    - Created a method to update stats in combat module with skill module.

--- Scripts/CommLib/ActorLibs.gd
+++ Scripts/CommLib/ActorLibs.gd
@@ -3,13 +3,46 @@ extends Node
 func get_speed_tracker() -> SpeedManager:
 	return SpeedManager.new()
 
-## Useful to calculate damage considering resitances and endurance.
-func calculate_damage(ActorAttack: float, ActorDefense: int, ActorEndurance: int) -> int:
-	var _damage: float
 
-	_damage = (ActorAttack * (ActorAttack/(ActorAttack + ActorDefense))) * (1.0 - (ActorEndurance/100.0))
+## Useful to calculate damage. The attacker MUST have a combat module.
+func calculate_damage_with_combat(AttackerCombat: VitalityCombat, DefenderCombat: VitalityCombat = null) -> int:
+	var _return_damage: int = 0
 	
-	return maxi(ceili(_damage), 1)
+	var _damage : float
+	var _resistance: float = 0
+	
+	for attack_type in AttackerCombat.attack_types.keys():
+		if GameProperties.AttackTypes[attack_type] == GameProperties.DamageTypes.PHYSICAL:
+			_damage = ActorLibs.calculate_statf(AttackerCombat.attack_types[attack_type] + AttackerCombat.skill_change_damage_physical, AttackerCombat.mod_damage_physical, AttackerCombat.mult_damage_physical)
+			if DefenderCombat:
+				_resistance = DefenderCombat.defense_physical
+		elif GameProperties.AttackTypes[attack_type] == GameProperties.DamageTypes.MAGICAL:
+			_damage = ActorLibs.calculate_statf(AttackerCombat.attack_types[attack_type] + AttackerCombat.skill_change_damage_magical, AttackerCombat.mod_damage_magical, AttackerCombat.mult_damage_magical)
+			if DefenderCombat:
+				_resistance = DefenderCombat.defense_magical
+		elif GameProperties.AttackTypes[attack_type] == GameProperties.DamageTypes.TRUE:
+			_damage = AttackerCombat.attack_types[attack_type]
+			_resistance = 0
+		
+		if attack_type in AttackerCombat.affinities:
+			_damage += AttackerCombat.affinities[attack_type] * _damage
+		
+		if DefenderCombat:
+			if attack_type in DefenderCombat.resistances:
+				_damage -= DefenderCombat.resistances[attack_type] * _damage
+		
+		_return_damage += maxi(roundi(_damage - _resistance), 0)
+	
+	return _return_damage
+
+
+# Called by vitality manager when skills are updated and combat module exists.
+func vitality_combat_update_with_skills(CombatModule: VitalityCombat, SkillModule: VitalitySkill) -> void:
+	CombatModule.skill_change_defense_magical = roundi(CombatModule.base_defense_magical * (SkillModule.endurance / 10))
+	CombatModule.skill_change_defense_physical = roundi(CombatModule.base_defense_physical * (SkillModule.endurance / 10))
+	
+	CombatModule.skill_change_damage_magical = SkillModule.intelligence * 2
+	CombatModule.skill_change_damage_physical = SkillModule.strength * 2
 
 
 func calculate_statf(BaseStat: float, ModStat: float, MultStat: float) -> float:
