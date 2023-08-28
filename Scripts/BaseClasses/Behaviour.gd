1dbed551092f528a09b25054e193e01b282d74d2
Ketei <137458469+Ketei@users.noreply.github.com>
2023-08-28 13:05:18 -0600

    Created Pack & Reworked Behaviour
    Created class BehaviourPack.
    Behaviour Rework:
    - added a function set_target_node which will try to pass the target node to the behaviour. This is to try and make Behaviour objects standalone.
    - Removed argument to setup_behaviour as the target node is passed before that.
    - Added an is_default boolean. This is to load that behaviour when the module enters the scene.

--- Scripts/BaseClasses/Behaviour.gd
+++ Scripts/BaseClasses/Behaviour.gd
@@ -1,8 +1,26 @@
 extends Node
 class_name Behaviour
 
+signal change_behaviour(BehaviourPackTarget: String, BehaviourTarget: String)
+
+# Set to true if the behaviour is to be loaded as default. This is only relevant for packs that
+# are loaded as default/initial.
+var is_default: bool = false
+
+# This is the type of behaviour this is, it's unique inside each ModuleBehaviour.
+# Some common behaviours id's are: "movement", "action", "weapon"
+var behaviour_id: String = ""
 var behaviour_module: ModuleBehaviour
 
+# When a behaviour is connected it can transition to other behaviours. Disconnecting the
+# behaviour means that this behaviour will ignore all transition signals. Useful if you
+# want to force a state no matter what.
+var behaviour_connected: bool = true
+
+# Use a variable of a specific class to target all changes to an object and retain predictions. 
+# Override the function set_target_node to set up your target node of the desired type.
+# Example: If your object variable object is of type Player, then override with:
+# if NewTargetNode is Player -> target_node = NewTargetNode.
 
 func enter(_args:= {}):
 	pass
@@ -21,4 +39,8 @@ func handle_physics(_delta : float) -> void:
 
 
 func setup_behaviour() -> void:
+	pass
+
+
+func set_target_node(NewTargetNode) -> void:
 	pass
