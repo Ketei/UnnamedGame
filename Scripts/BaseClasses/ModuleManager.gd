7020715aa325715618b3597e721e342fdf2c5629
Ketei <137458469+Ketei@users.noreply.github.com>
2023-08-30 17:12:35 -0600

    Animation change func
    Added function to change animation if behaviour module is loaded

--- Scripts/BaseClasses/ModuleManager.gd
+++ Scripts/BaseClasses/ModuleManager.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name ModuleManager
 
+signal change_animation(AnimPack: String, AnimAction: String)
+
 @export var parent_node: Node
 # ModuleTypeName: ModuleID: ModuleNode
 var _loaded_modules: Dictionary = {}
@@ -8,24 +10,41 @@ var _loaded_modules: Dictionary = {}
 
 func _ready():
 	for child in self.get_children():
-		if child is Module or child is ModuleAnimationPlayer:
-			if _loaded_modules.has(child.module_type.to_lower()):
-				print_debug("This actor already has a " + child.module_type + " module.")
-				print_debug(child.module_type + " will be set to be " + child.name)		
-			_loaded_modules[child.module_type.to_lower()] = child
-			child.module_manager = self
-			child.set_up_module()
+		if not child is Module or not child is ModuleAnimationPlayer:
+			continue
+		
+		warn_if_repeated_modules(child.module_type)
+		
+		_loaded_modules[child.module_type] = child
+		child.module_manager = self
+		child.set_up_module()
+		
+		if child is ModuleBehaviour:
+			child.change_animation.connect(_change_animation)
 
 
 ## Returns true if the module is present and loaded
 func has_module(ModuleName: String) -> bool:
-	return _loaded_modules.has(ModuleName.to_lower())
+	return _loaded_modules.has(ModuleName)
 
 
 ## Returns the reference to access the module from outside of the manager.
 func get_module(ModuleName: String):
-	var return_module = null
-	if _loaded_modules.has(ModuleName.to_lower()):
-		return_module = _loaded_modules[ModuleName.to_lower()]
+	if _loaded_modules.has(ModuleName):
+		return _loaded_modules[ModuleName]
+	else:
+		return null
+
+
+func warn_if_repeated_modules(ModuleType: String) -> void:
+	if _loaded_modules.has(ModuleType):
+		print_debug("This actor already has a " + ModuleType + " module.")
+		print_debug(ModuleType + " will be replaced")		
+
+
+func _change_animation(Pack: String, Action: String, PlayRandom: bool):
+	if not has_module("animation-player"):
+		return
 	
-	return return_module
+	get_module("animation-player").custom_play(Pack, Action, PlayRandom)
+
