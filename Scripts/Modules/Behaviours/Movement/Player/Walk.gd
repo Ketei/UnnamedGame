3afe8fe128cd095115b9b474d89ad8c45a04f8ce
Ketei <137458469+Ketei@users.noreply.github.com>
2023-09-01 15:17:25 -0600

    Create Run.gd & Walk.gd

---
+++ Scripts/Modules/Behaviours/Movement/Player/Walk.gd
@@ -0,0 +1,62 @@
+extends Behaviour
+
+var actor: Player
+var _axis_direction: float
+
+func enter(_args:= {}):
+	if not actor:
+		return
+	
+	if actor.movement_status == actor.MovementSpeed.CROUCH:
+		change_animation.emit("movement-ground", "walk-crouch")
+	elif actor.movement_status == Actor.MovementSpeed.WALK:
+		change_animation.emit("movement-ground", "walk")
+
+
+func handle_key_input(event: InputEvent) -> void:
+	if not actor:
+		return
+	
+	if event.is_action_pressed("gc_crouch"):
+		actor.is_crouching = not actor.is_crouching
+
+		if actor.movement_status == actor.MovementSpeed.CROUCH:
+			change_animation.emit("movement-ground", "walk-crouch")
+		elif actor.movement_status == actor.MovementSpeed.WALK:
+			change_animation.emit("movement-ground", "walk")
+		elif actor.movement_status == actor.MovementSpeed.RUN:
+			change_behaviour.emit("movement", "run")
+	
+	if event.is_action_pressed("gc_walk"):
+		actor.is_walking = not actor.is_walking
+		
+		if actor.movement_status == actor.MovementSpeed.RUN:
+			change_behaviour.emit("movement", "run")
+
+
+func handle_physics(delta : float) -> void:
+	if not actor:
+		return
+	
+	_axis_direction = Input.get_axis("gc_left", "gc_right")
+	
+	if _axis_direction == 0 and actor.velocity.x == 0:
+		change_behaviour.emit("movement", "idle")
+		return
+	
+	set_facing_direction()
+	actor.change_actor_speed(_axis_direction, delta)
+
+
+func setup_behaviour() -> void:
+	behaviour_id = "walk"
+
+
+func set_target_node(NewTargetNode) -> void:
+	if NewTargetNode is Player:
+		actor = NewTargetNode
+
+
+func set_facing_direction():
+	if _axis_direction != 0:
+		actor.set_facing_right(0 < actor.velocity.x)
