2b463d96ebb1e28c63c049e212e1a66b71b910f4
Ketei <137458469+Ketei@users.noreply.github.com>
2023-09-02 18:52:38 -0600

    Simplified math
    Simplified get_jump_* and get_normal_gravity math

--- Scripts/CommLib/QuickMath.gd
+++ Scripts/CommLib/QuickMath.gd
@@ -8,7 +8,7 @@ func get_jump_velocity(JumpInTiles: float, JumpTimeToPeak: float) -> float:
 		return 0.0
 	else:
 		# The +1 comes from the single pixel that is eaten by the floor before jump
-		return ((2.0 * ((JumpInTiles * GameProperties.grid_size) + 1)) / JumpTimeToPeak) * -1.0
+		return -((2.0 * ((JumpInTiles * GameProperties.grid_size) + 1)) / JumpTimeToPeak)
 
 
 func get_jump_gravity(JumpInTiles:float, JumpTimeToPeak:float) -> float:
@@ -17,7 +17,7 @@ func get_jump_gravity(JumpInTiles:float, JumpTimeToPeak:float) -> float:
 		print_debug("Warning: get_jump_gravity() cannot be performed successfully. Returning 0.0 instead")
 		return 0.0
 	else:
-		return ((-2.0 * (JumpInTiles * GameProperties.grid_size)) / pow(JumpTimeToPeak, 2)) * -1.0
+		return -((-2.0 * (JumpInTiles * GameProperties.grid_size)) / pow(JumpTimeToPeak, 2))
 
 
 func get_normal_gravity(JumpInTiles:float, JumpTimeToFloor:float) -> float:
@@ -26,7 +26,7 @@ func get_normal_gravity(JumpInTiles:float, JumpTimeToFloor:float) -> float:
 		print_debug("Warning: get_normal_gravity() cannot be performed successfully. Returning 0.0 instead")
 		return 0.0
 	else:
-		return ((-2.0 * (JumpInTiles * GameProperties.grid_size)) / pow(JumpTimeToFloor, 2)) * -1.0
+		return -((-2.0 * (JumpInTiles * GameProperties.grid_size)) / pow(JumpTimeToFloor, 2))
 
 ## Checks if the first value is between the two values given. If it is returns true else returns false
 func is_between(Value : float, From : float, To : float) -> bool:
