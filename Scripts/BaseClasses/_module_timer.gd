72b199f3c4365c62310391cae7601ffe1fe6f9c8
Ketei <137458469+Ketei@users.noreply.github.com>
2023-09-04 20:20:55 -0600

    Referec count added

--- Scripts/BaseClasses/_module_timer.gd
+++ Scripts/BaseClasses/_module_timer.gd
@@ -4,7 +4,10 @@ class_name TimerForModule
 signal timer_timeout(timer_name)
 
 ## If true the timer node won't be freed by the Timers Manager once this timer reaches timeout.
-@export var is_persistent: bool = false
+var is_persistent: bool = false
+var reference_count: int = 0 :
+	set(value):
+		reference_count = maxi(value, 0)
 
 
 func _ready():
