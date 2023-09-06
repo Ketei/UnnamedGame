a1269aeef1a222bed63ba00be51e94df25f89ac9
Ketei <137458469+Ketei@users.noreply.github.com>
2023-09-06 16:19:25 -0600

    Added axis variable
    Along with a function to update it

--- Scripts/Actors/Player.gd
+++ Scripts/Actors/Player.gd
@@ -4,6 +4,14 @@ class_name Player
 # TrackerName(str): InteractTracker
 # Add proper keys once finished: up, down, left, etc.
 var interact_tracker: Dictionary = {}
+var axis_strenght: Vector2 = Vector2(0, 0)
+
+
+func update_input_axis(UpdateAxisX: bool = true, UpdateAxisY: bool = true) -> void:
+	if UpdateAxisX:
+		axis_strenght.x = Input.get_axis("gc_left","gc_right")
+	if UpdateAxisY:
+		axis_strenght.y = Input.get_axis("gc_up", "gc_down")
 
 
 func add_interact_tracker(TrackerKey: String):
