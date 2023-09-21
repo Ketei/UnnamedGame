1f40e83e7adc9ab493466d30d5336fdc901cf398
Ketei <ketei.ilust@gmail.com>
2023-09-21 16:44:43 -0600

    More names changed

--- Scripts/Modules/ModuleInteraction.gd
+++ Scripts/Modules/ModuleInteraction.gd
@@ -1,8 +1,8 @@
+class_name ModuleInteract
 extends Module
 ## This module makes interaction between modules possible (and safe).
 ## It'll only include modules that have cross-module interaction by nature.
 ## This module only works properly if the other modules use the standard structure
-class_name ModuleInteract
 
 ## Modules that can be interacted with
 @export_group("Interacted Modules")
@@ -20,9 +20,9 @@ func _ready():
 
 
 func set_up_module() -> void:
-	terrain_tracker.terrain_changed.connect(_terrain_tracker_changed)
+	terrain_tracker.terrain_changed.connect(__terrain_tracker_changed)
 	is_module_enabled = true
 
 # Changes the animation terrain state depending on the terrain state of terrain module
-func _terrain_tracker_changed(NewState: GameProperties.TerrainState) -> void:
+func __terrain_tracker_changed(NewState: GameProperties.TerrainState) -> void:
 	animation_player.set_anim_state("root", GameProperties.TerrainNames[NewState])
