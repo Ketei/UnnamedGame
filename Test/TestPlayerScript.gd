b31432f58d09a45939046ca4c63504a4525024f6
Ketei <ketei.ilust@gmail.com>
2023-09-22 11:37:47 -0600

    Spike added
    Ouchies.
    It damages you 5 hp per second.

--- Test/TestPlayerScript.gd
+++ Test/TestPlayerScript.gd
@@ -1,8 +1,28 @@
-extends CharacterBody2D
+extends Area2D
 
-const move_speed = 2 * 64
+@onready var hp: ComponentStat = $"../ModuleManager/ModuleStats/Health"
+@onready var hurtbox: CollisionShape2D = $CollisionShape2D
+var timer: Timer
 
 func _physics_process(delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * move_speed
-	move_and_slide()
+	pass
+
+
+func _ready():
+	timer = Timer.new()
+	timer.one_shot = true
+	timer.autostart = false
+	timer.wait_time = 1.0
+	timer.timeout.connect(_timer_timeout)
+	self.add_child(timer)
+
+
+func _on_area_entered(area):
+	hp.change_current(-5)
+	hurtbox.set_deferred("disabled", true)
+	#hurtbox.disabled = true
+	timer.start()
+
+
+func _timer_timeout() -> void:
+	hurtbox.set_deferred("disabled", false)
