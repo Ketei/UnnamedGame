a167709db48f1ac3d64e300cf28bcf3a09545609
Ketei <ketei.ilust@gmail.com>
2023-09-22 11:38:33 -0600

    Player can now self-heal

--- Scripts/Actors/Player.gd
+++ Scripts/Actors/Player.gd
@@ -5,7 +5,7 @@ extends Actor
 var axis_strength: Vector2 = Vector2(0, 0)
 
 # Toggles
-
+@onready var hp: ComponentStat = $ModuleManager/ModuleStats/Health
 # This will give the option to switch between walk & run with hold or a press.
 # This is to be moved to settings once it's fully implemented
 var walk_hold: bool = false
@@ -17,3 +17,7 @@ func update_input_axis(update_x_axis := true, update_y_axis := true) -> void:
 	if update_y_axis:
 		axis_strength.y = Input.get_axis("gc_up", "gc_down")
 
+
+func _unhandled_key_input(event):
+	if event.is_action_pressed("ui_text_backspace"):
+		hp.change_current(25)
