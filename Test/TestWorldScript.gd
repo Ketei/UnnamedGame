509132df669453d41812a1513fb64b91d9c1a34f
Ketei <ketei.ilust@gmail.com>
2023-09-27 15:07:24 -0600

    Update TestWorldScript.gd
    This is now done by game singleton

--- Test/TestWorldScript.gd
+++ Test/TestWorldScript.gd
@@ -1,22 +1,7 @@
 extends Node2D
 
-
-func _init():
-	update_physics_rate()
-
-
 # This is set-up to see if I can fix jitter on 60hz+ screens.
 # Apparently this works good enough.
-func update_physics_rate() -> void:
-	if Settings.refresh_rate <= 0:
-		Settings.refresh_rate = int(DisplayServer.screen_get_refresh_rate())
-
-	if Settings.refresh_rate == -1:
-		print_debug("Couldn't automatically detect the monitor refresh rate")
-		return
-	
-	Engine.physics_ticks_per_second = Settings.refresh_rate
-	Engine.max_fps = Settings.refresh_rate
 
 
 func _unhandled_key_input(event):
