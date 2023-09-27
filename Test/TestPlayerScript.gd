8510cb9ac048a644d8d470953e67cd854580e5e5
Ketei <ketei.ilust@gmail.com>
2023-09-27 15:07:36 -0600

    Update TestPlayerScript.gd
    Testin

--- Test/TestPlayerScript.gd
+++ Test/TestPlayerScript.gd
@@ -1,26 +1,32 @@
 extends Area2D
 
+
+var invis_frames: float = 2.0
+
 @onready var hp: ComponentStat = $"../ModuleManager/ModuleStats/Health"
 @onready var hurtbox: CollisionShape2D = $CollisionShape2D
-var timer: Timer
+@onready var player_sprite : ActorSprite2D = $"../ActorSprite"
+@onready var behaviour_module: ModuleBehaviour = $"../ModuleManager/ModuleBehaviour"
+@onready var player: Player = $".."
 
-func _physics_process(delta):
-	pass
+var timer: Timer
 
 
 func _ready():
 	timer = Timer.new()
 	timer.one_shot = true
 	timer.autostart = false
-	timer.wait_time = 1.0
+	timer.wait_time = invis_frames
 	timer.timeout.connect(_timer_timeout)
 	self.add_child(timer)
 
 
-func _on_area_entered(area):
+func _on_area_entered(_area):
+	behaviour_module.change_behaviour("movement/hurt")
+	player.velocity.x = 0.0
 	hp.change_current(-5)
 	hurtbox.set_deferred("disabled", true)
-	#hurtbox.disabled = true
+	player_sprite.play_effect(ActorSprite2D.EffectType.BLINK, invis_frames)
 	timer.start()
 
 
