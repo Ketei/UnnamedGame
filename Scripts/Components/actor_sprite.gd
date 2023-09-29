bd71b624a99353edec0eec446117734e3bb78686
Ketei <ketei.ilust@gmail.com>
2023-09-29 14:40:57 -0600

    Update actor_sprite.gd
    Created signals and functions for flipping.
    This is for hit/hurt boxes

--- Scripts/Components/actor_sprite.gd
+++ Scripts/Components/actor_sprite.gd
@@ -2,14 +2,29 @@ class_name ActorSprite2D
 extends Sprite2D
 
 
+signal sprite_flipped(axis_char: String, is_flipped: bool)
+
 enum EffectType {BLINK}
 
 const _EFFECT_NAMES: Dictionary = {
 	"0": "blink"
 }
 
+
 @onready var _effect_player: AnimationPlayer = %SpriteEffects
 @onready var _effect_timer: Timer = %EffectTimer
+
+
+func flip_vertical(flip_vertical: bool) -> void:
+	if flip_vertical != flip_v:
+		flip_v = flip_vertical
+		sprite_flipped.emit("v", flip_v)
+
+
+func flip_horizontal(flip_horizontal: bool) -> void:
+	if flip_horizontal != flip_h:
+		flip_h = flip_horizontal
+		sprite_flipped.emit("h", flip_h)
 
 
 func play_effect(effect_type: EffectType, effect_time: float) -> void:
@@ -19,4 +34,5 @@ func play_effect(effect_type: EffectType, effect_time: float) -> void:
 
 func _on_timer_timeout():
 	_effect_player.stop()
+
 
